7a63ad9380e54de322887b40f71bebb4d314f826
ChoSangmuk <tkdanr612@gmail.com>
2022-04-29 23:20:00 +0900

    jpa start1 프로젝트 생성 및 폴더 구조 변경

---
+++ JavaPersistenceAPI/jpa-start1/src/main/java/com/example/jpastart1/JpaStart1Application.java
@@ -0,0 +1,13 @@
+package com.example.jpastart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaStart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaStart1Application.class, args);
+	}
+
+}
